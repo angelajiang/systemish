682e78cc051fd2a1b709e5ae819d8620d4c063aa
Anuj Kalia <anujkaliaiitd@gmail.com>
2014-11-13 14:57:32 -0500

    Update memory-capacity/rand-rate

--- memory-capacity/rand-rate.sh
+++ memory-capacity/rand-rate.sh
@@ -2,5 +2,5 @@ shm-rm.sh
 for i in `seq 0 7`; do
 	tid=`expr $i + 1`
 	core=`expr 2 \* $i`
-	sudo taskset -c $core ./rand-rate $tid &
+	sudo numactl --physcpubind 0,2,4,6,8,10,12,14 --membind 0 ./rand-rate $tid &
 done
