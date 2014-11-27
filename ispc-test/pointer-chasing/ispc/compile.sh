a6f8d9013e21f25fdb7d651cafd07c05baf36203
Anuj Kalia <anujkaliaiitd@gmail.com>
2014-11-27 00:16:41 -0500

    Start working on ISPC pointer-chasing

--- ispc-test/pointer-chasing/ispc/compile.sh
+++ ispc-test/pointer-chasing/ispc/compile.sh
@@ -1,9 +1,13 @@
+ISPC_HOME="/home/akalia/ispc-v1.8.0-linux"
+
 # Remove array A's shm-id
 sudo ipcrm -M 3185
+
+rm main
 
 # Create C++ file
 ispc main.ispc --emit-c++ --cpu=corei7-avx --target=generic-16 -o main-ispc.cpp --c++-include-file=generic-16.h
 
-gcc -O3 -I ~/ISPC/ispc/examples/intrinsics/ main-ispc.cpp main.c -o main -lrt -lm
+gcc -O3 -I $ISPC_HOME/examples/intrinsics/ main-ispc.cpp main.c -o main -lrt -lm
 
 
