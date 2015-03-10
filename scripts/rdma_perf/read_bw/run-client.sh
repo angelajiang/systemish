f86cd097011cc086a6f17986a8cf4e14e477df8c
Anuj Kalia <anujkaliaiitd@gmail.com>
2015-03-10 04:47:24 -0500

    Improve client script

--- scripts/rdma_perf/read_bw/run-client.sh
+++ scripts/rdma_perf/read_bw/run-client.sh
@@ -16,16 +16,20 @@ client_id=$1
 blue "Pulling latest client script"
 git pull
 
+options="--size=32 --run_infinitely --duration=1 --post-list=16"
+server="10.113.211.51"
+
 # Connect to servers on IB port 1
 ib_p1_port1=`expr 20000 + 2 \* $client_id`
 ib_p1_port2=`expr 20000 + 2 \* $client_id + 1`
 blue "Connecting to IB port 1 via socket ports $ib_p1_port1 and $ib_p1_port2"
-taskset -c 0 ib_read_bw --ib-port=1 --size=32 --run_infinitely --duration=1 --port=$ib_p1_port1 10.113.211.51 &
-taskset -c 2 ib_read_bw --ib-port=1 --size=32 --run_infinitely --duration=1 --port=$ib_p1_port2 10.113.211.51 &
+
+taskset -c 0 ib_read_bw $options --ib-port=1 --port=$ib_p1_port1 $server &
+taskset -c 0 ib_read_bw $options --ib-port=1 --port=$ib_p1_port2 $server &
 
 # Connect to servers on IB port 2
 ib_p2_port1=`expr 30000 + 2 \* $client_id`
 ib_p2_port2=`expr 30000 + 2 \* $client_id + 1`
 blue "Connecting to IB port 2 via socket ports $ib_p2_port1 and $ib_p2_port2"
-taskset -c 4 ib_read_bw --ib-port=2 --size=32 --run_infinitely --duration=1 --port=$ib_p2_port1 10.113.211.51 &
-taskset -c 6 ib_read_bw --ib-port=2 --size=32 --run_infinitely --duration=1 --port=$ib_p2_port2 10.113.211.51 &
+taskset -c 0 ib_read_bw $options --ib-port=2 --port=$ib_p2_port1 $server &
+taskset -c 0 ib_read_bw $options --ib-port=2 --port=$ib_p2_port2 $server &
