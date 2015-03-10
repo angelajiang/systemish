fe81bd29184cb0cf2eabbe12570c07de13ef77d5
Anuj Kalia <anujkaliaiitd@gmail.com>
2015-03-10 04:53:37 -0500

    Increase number of server and client processes

--- scripts/rdma_perf/read_bw/run-client.sh
+++ scripts/rdma_perf/read_bw/run-client.sh
@@ -22,14 +22,22 @@ server="10.113.211.51"
 # Connect to servers on IB port 1
 ib_p1_port1=`expr 20000 + 2 \* $client_id`
 ib_p1_port2=`expr 20000 + 2 \* $client_id + 1`
-blue "Connecting to IB port 1 via socket ports $ib_p1_port1 and $ib_p1_port2"
+ib_p1_port3=`expr 20000 + 2 \* $client_id + 2`
+ib_p1_port4=`expr 20000 + 2 \* $client_id + 3`
+blue "Connecting to IB port 1 via socket ports $ib_p1_port1, $ib_p1_port2, $ib_p1_port3, $ib_p1_port4"
 
 taskset -c 0 ib_read_bw $options --ib-port=1 --port=$ib_p1_port1 $server &
-taskset -c 0 ib_read_bw $options --ib-port=1 --port=$ib_p1_port2 $server &
+taskset -c 2 ib_read_bw $options --ib-port=1 --port=$ib_p1_port2 $server &
+taskset -c 4 ib_read_bw $options --ib-port=1 --port=$ib_p1_port3 $server &
+taskset -c 6 ib_read_bw $options --ib-port=1 --port=$ib_p1_port4 $server &
 
 # Connect to servers on IB port 2
 ib_p2_port1=`expr 30000 + 2 \* $client_id`
 ib_p2_port2=`expr 30000 + 2 \* $client_id + 1`
-blue "Connecting to IB port 2 via socket ports $ib_p2_port1 and $ib_p2_port2"
-taskset -c 0 ib_read_bw $options --ib-port=2 --port=$ib_p2_port1 $server &
-taskset -c 0 ib_read_bw $options --ib-port=2 --port=$ib_p2_port2 $server &
+ib_p2_port3=`expr 30000 + 2 \* $client_id + 2`
+ib_p2_port4=`expr 30000 + 2 \* $client_id + 3`
+blue "Connecting to IB port 2 via socket ports $ib_p2_port1, $ib_p2_port2, $ib_p2_port3, $ib_p2_port4"
+taskset -c 8 ib_read_bw $options --ib-port=2 --port=$ib_p2_port1 $server &
+taskset -c 10 ib_read_bw $options --ib-port=2 --port=$ib_p2_port2 $server &
+taskset -c 12 ib_read_bw $options --ib-port=2 --port=$ib_p2_port3 $server &
+taskset -c 14 ib_read_bw $options --ib-port=2 --port=$ib_p2_port4 $server &
--- scripts/rdma_perf/read_bw/run-server.sh
+++ scripts/rdma_perf/read_bw/run-server.sh
@@ -1,11 +1,11 @@
 # Run servers on port #1
-for i in `seq 0 9`; do
+for i in `seq 0 19`; do
 	port=`expr 20000 + $i`
 	ib_read_bw --ib-port=1 --size=32 --run_infinitely --duration=1 --port=$port &
 done
 
 # Run servers on port #1
-for i in `seq 0 9`; do
+for i in `seq 0 19`; do
 	port=`expr 30000 + $i`
 	ib_read_bw --ib-port=2 --size=32 --run_infinitely --duration=1 --port=$port &
 done
