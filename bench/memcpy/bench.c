3833c7f44de2f2d54e761abc9855d364a7bf814a
Anuj Kalia <akalia@cs.cmu.edu>
2015-11-16 15:00:58 -0700

    Add memcpy bench

---
+++ bench/memcpy/bench.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<time.h>
+
+#define NUM_PKTS (16 * 1024 * 1024)
+#define NUM_LONGS 10
+
+int main(int argc, char **argv)
+{
+	printf("Starting computing hashes\n");
+	struct timespec start, end;
+	clock_gettime(CLOCK_REALTIME, &start);
+
+	long long sum = 0;
+	int i = 0, j = 0;
+	long long A[NUM_LONGS] = {0};
+	long long B[NUM_LONGS] = {0};
+
+	for(i = 0; i < NUM_PKTS; i ++) {
+		memcpy(B, A, NUM_LONGS * sizeof(long long));
+		memcpy(A, B, NUM_LONGS * sizeof(long long));
+	}
+
+	// GCC is so clever, so print all
+	for(j = 0; j < NUM_LONGS; j++) {
+		printf("%lld\n", A[j]);
+	}
+
+	clock_gettime(CLOCK_REALTIME, &end);
+
+	double seconds = (end.tv_sec - start.tv_sec) + 
+		(double) (end.tv_nsec - start.tv_nsec) / 1000000000;
+	double nanoseconds = (end.tv_sec - start.tv_sec) * 1000000000 + 
+		(end.tv_nsec - start.tv_nsec);
+
+	printf("Time = %f, time per memcpy = %f ns, sum = %lld\n", 
+		seconds, nanoseconds / (NUM_PKTS * 2), sum);
+
+	return 0;
+}
--- scripts/shm-rm.sh
+++ scripts/shm-rm.sh
@@ -1,22 +1,6 @@
-#!/bin/bash
-
-ME=`whoami`
-
-IPCS_S=`ipcs -s | egrep "0x[0-9a-f]+ [0-9]+" | grep $ME | cut -f2 -d" "`
-IPCS_M=`ipcs -m | egrep "0x[0-9a-f]+ [0-9]+" | grep $ME | cut -f2 -d" "`
-IPCS_Q=`ipcs -q | egrep "0x[0-9a-f]+ [0-9]+" | grep $ME | cut -f2 -d" "`
-
-
-for id in $IPCS_M; do
-  ipcrm -m $id;
+for i in `seq 0 64`; do
+	sudo ipcrm -M $i
 done
-
-for id in $IPCS_S; do
-  ipcrm -s $id;
-done
-
-for id in $IPCS_Q; do
-  ipcrm -q $id;
-done
-
-# Script from http://stackoverflow.com/questions/2143404/delete-all-shared-memory-and-semaphores-on-linux
+sudo ipcrm -M 3185		#Writer's hash table
+sudo ipcrm -M 3186		#Writer's hash table
+sudo ipcrm -M 3006		#Writer's hash table
