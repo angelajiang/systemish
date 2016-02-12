0271fcb24ae8ac52773fbd1f3b3d7161ce27520b
Anuj Kalia <anujkaliaiitd@gmail.com>
2016-02-12 17:32:07 -0500

    Add symmetric coroutine switching example

--- boost/coro.cpp
+++ boost/coro.cpp
@@ -3,17 +3,33 @@
 
 using namespace boost::coroutines;
 
-void cooperative(coroutine<void>::push_type &sink)
+int i = 0;
+
+boost::coroutines::symmetric_coroutine<void>::call_type* other_a = 0, * other_b = 0;
+
+boost::coroutines::symmetric_coroutine<void>::call_type coro_a(
+    [&](boost::coroutines::symmetric_coroutine<void>::yield_type& yield)
 {
-	std::cout << "Hello";
-	sink();
-	std::cout << "world";
-}
+	while(i < 10) {
+		printf("a yield to b, i = %d\n", i);
+		i++;
+		yield(*other_b);    // yield to coroutine coro_b
+	}
+});
+
+boost::coroutines::symmetric_coroutine<void>::call_type coro_b(
+    [&](boost::coroutines::symmetric_coroutine<void>::yield_type& yield)
+{
+	while(i < 10) {
+		printf("b yield to a, i = %d\n", i);
+		i++;
+		yield(*other_a);    // yield to coroutine coro_b
+	}
+});
 
 int main()
 {
-	coroutine<void>::pull_type source {cooperative};
-	std::cout << ", ";
-	source();
-	std::cout << "!\n";
+	other_a = &coro_a;
+	other_b = &coro_b;
+	coro_a(); // enter coroutine-fn of coro_a
 }
