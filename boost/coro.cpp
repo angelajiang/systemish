3fff50943eabc0d7151e77d8f6932e5e5355d21e
Anuj Kalia <anujkaliaiitd@gmail.com>
2016-02-12 17:42:01 -0500

    Add benchmark to measure coroutine switch performance

--- boost/coro.cpp
+++ boost/coro.cpp
@@ -1,17 +1,19 @@
 #include <boost/coroutine/all.hpp>
 #include <iostream>
+#include <time.h>
+
+#define MAX 10000000
 
 using namespace boost::coroutines;
-
 int i = 0;
+
 
 boost::coroutines::symmetric_coroutine<void>::call_type* other_a = 0, * other_b = 0;
 
 boost::coroutines::symmetric_coroutine<void>::call_type coro_a(
     [&](boost::coroutines::symmetric_coroutine<void>::yield_type& yield)
 {
-	while(i < 10) {
-		printf("a yield to b, i = %d\n", i);
+	while(i < MAX) {
 		i++;
 		yield(*other_b);    // yield to coroutine coro_b
 	}
@@ -20,8 +22,9 @@ boost::coroutines::symmetric_coroutine<void>::call_type coro_a(
 boost::coroutines::symmetric_coroutine<void>::call_type coro_b(
     [&](boost::coroutines::symmetric_coroutine<void>::yield_type& yield)
 {
-	while(i < 10) {
-		printf("b yield to a, i = %d\n", i);
+	clock_gettime(CLOCK_REALTIME, &timer_start);
+
+	while(i < MAX) {
 		i++;
 		yield(*other_a);    // yield to coroutine coro_b
 	}
@@ -29,7 +32,19 @@ boost::coroutines::symmetric_coroutine<void>::call_type coro_b(
 
 int main()
 {
+	struct timespec timer_start, timer_end;
+
 	other_a = &coro_a;
 	other_b = &coro_b;
+
 	coro_a(); // enter coroutine-fn of coro_a
+
+	clock_gettime(CLOCK_REALTIME, &timer_end);
+	assert(i == MAX);
+
+	double ns = (timer_end.tv_sec - timer_start.tv_sec) * 1000000000 +
+		(double) (timer_end.tv_nsec - timer_start.tv_nsec);
+	printf("main: Time = %.2f ns, context switch time = %.2f ns\n",
+		ns, ns / MAX);
+
 }
