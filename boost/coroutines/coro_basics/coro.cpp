ae4d67941c2139896d46e3f1eedd6a0ee25cf7f5
Anuj Kalia <anujkaliaiitd@gmail.com>
2016-03-05 06:53:36 -0500

    Add arguments to coroutines

--- boost/coroutines/coro_basics/coro.cpp
+++ boost/coroutines/coro_basics/coro.cpp
@@ -1,3 +1,4 @@
+#include <boost/bind.hpp>
 #include <boost/coroutine/all.hpp>
 #include <iostream>
 #include <chrono>
@@ -5,22 +6,25 @@
 #define NUM_CORO 8
 #define NUM_CORO_ 7
 
-#define NUM_SWITCHES_PER_THREAD 10000000	/* 10 million */
-#define NUM_SWITCHES (NUM_CORO * NUM_SWITCHES_PER_THREAD)
+#define NUM_SWITCHES_PER_CORO 10000000	/* 10 million */
+#define NUM_SWITCHES (NUM_CORO * NUM_SWITCHES_PER_CORO)
+
+#define DEBUG 0
 
 using namespace std;
 using namespace std::chrono;
 using namespace boost::coroutines;
-int coro_id = 0;
 
-boost::coroutines::symmetric_coroutine<void>::call_type *coro_arr[NUM_CORO];
+boost::coroutines::symmetric_coroutine<void>::call_type coro_arr[NUM_CORO];
 
 /*
  * The function executed by each coroutine. @switch_i represents the number of
  * switches executed until now.
  */
-void coro_func(boost::coroutines::symmetric_coroutine<void>::yield_type& yield)
+void coro_func(boost::coroutines::symmetric_coroutine<void>::yield_type& yield,
+	int coro_id)
 {
+	cout << "coro_func: In coroutine " << coro_id << endl;
 	int switch_i = 0;
 
 	int var_1 = 0, var_2 = 0, var_3 = 0;
@@ -28,7 +32,7 @@ void coro_func(boost::coroutines::symmetric_coroutine<void>::yield_type& yield)
 	long long buf_2[1024] = {0};
 	double fpu_checker = 0.0;
 
-	while(switch_i < NUM_SWITCHES_PER_THREAD) {
+	while(switch_i < NUM_SWITCHES_PER_CORO) {
 		var_1++;
 		var_2 += var_1;
 	
@@ -39,8 +43,12 @@ void coro_func(boost::coroutines::symmetric_coroutine<void>::yield_type& yield)
 
 		switch_i++;
 
-		coro_id = (coro_id + 1) & (NUM_CORO - 1);
-		yield(*coro_arr[coro_id]);
+		int next_coro_id = (coro_id + 1) & (NUM_CORO - 1);
+#if DEBUG == 1
+		cout << "coro_func: Switching from coro " << coro_id << " to " <<
+			next_coro_id << endl;
+#endif
+		yield(coro_arr[next_coro_id]);
 	}
 
 	printf("var_1 = %d, var_2 = %d, buf_1[1023] = %lld, buf_2[511] = %lld\n"
@@ -57,25 +65,21 @@ void test()
 
 	/* Create @NUM_CORO coroutines */
 	for(int coro_i = 0; coro_i < NUM_CORO; coro_i++) {
+		cout << "test: Init coro " << coro_i << endl;
 		coro_arr[coro_i] =
-			new boost::coroutines::symmetric_coroutine<void>::call_type(
-				coro_func, boost::coroutines::attributes(fpu_flag));
+			boost::coroutines::symmetric_coroutine<void>::call_type(
+				boost::bind(coro_func, _1, coro_i),
+				boost::coroutines::attributes(fpu_flag));
 	}
 
 	/* Launch the 1st coroutine; this calls other coroutines later. */
-	(*coro_arr[0])();
+	(coro_arr[0])();
 
 	auto timer_end = high_resolution_clock::now();
-	printf("Coro ID = %d\n", coro_id);	/* Prevent optimization */
 
 	auto ns = duration_cast<nanoseconds>(timer_end - timer_start).count();
 	cout << "main: Time =  " << ns << ", context switch time = " <<
 		ns / NUM_SWITCHES << " ns" << endl;
-
-	/* Destroy the coroutines. */
-	for(int coro_i = 0; coro_i < NUM_CORO; coro_i++) {
-		delete coro_arr[coro_i];
-	}
 }
 
 int main()
@@ -89,5 +93,5 @@ int main()
 
 	test();
 
-	printf("sum = %d\n", sum);
+	printf("sum = %d\n", sum);	/* Prevent warmup optimization */
 }
