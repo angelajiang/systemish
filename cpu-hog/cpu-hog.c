c0b97643686ea5701093ebb6d8fabf5d650300ba
Anuj Kalia <anujkaliaiitd@gmail.com>
2016-03-16 03:39:41 -0400

    Duh

--- cpu-hog/cpu-hog.c
+++ cpu-hog/cpu-hog.c
@@ -1,12 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#define ITERS (1024 * 1024 * 1024)
 int main()
 {
-	int i, sum = 23;
-	for(i = 0; i < ITERS; i++) {
-		sum += i;
+	while(1) {
 	}
-	printf("sum = %d\n", sum);
 }
--- cpu-hog/run.sh
+++ cpu-hog/run.sh
@@ -1,2 +1,14 @@
-perf stat ./cpu-hog-add
-perf stat ./cpu-hog-mul
+taskset -c 0 ./cpu-hog &
+taskset -c 2 ./cpu-hog &
+taskset -c 4 ./cpu-hog &
+#taskset -c 6 ./cpu-hog &
+#taskset -c 8 ./cpu-hog &
+#taskset -c 10 ./cpu-hog &
+#taskset -c 12 ./cpu-hog &
+#taskset -c 14 ./cpu-hog &
+#taskset -c 16 ./cpu-hog &
+#taskset -c 18 ./cpu-hog &
+#taskset -c 20 ./cpu-hog &
+#taskset -c 22 ./cpu-hog &
+#taskset -c 24 ./cpu-hog &
+#taskset -c 26 ./cpu-hog &
