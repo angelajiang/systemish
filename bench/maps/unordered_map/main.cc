cc4556fa5561a841671ef761dccce5517a0631fc
Anuj Kalia <akalia@cs.cmu.edu>
2016-03-20 08:06:34 -0600

    Add boost unordered_map to benchmark

--- bench/maps/unordered_map/main.cc
+++ bench/maps/unordered_map/main.cc
@@ -5,11 +5,14 @@
 #include<assert.h>
 #include<stdint.h>
 #include<unordered_map>
+#include <boost/unordered_map.hpp>
 #include<chrono>
 using namespace std;
 using namespace std::chrono;
 
 #define KEYS 3000000  /* 30 million */
+
+#define USE_BOOST 1
 
 static inline uint32_t hrd_fastrand(uint64_t *seed)
 {
@@ -20,6 +23,8 @@ static inline uint32_t hrd_fastrand(uint64_t *seed)
 int main()
 {
 	long long sum = 1;
+	std::chrono::high_resolution_clock timer;
+
 	printf("Starting warmup\n");
 	// Warmup
 	for(int i = 0; i < 1000000000; i++) {
@@ -29,12 +34,22 @@ int main()
 
 	printf("Starting insertion\n");
 	std::unordered_map<uint64_t, uint64_t> M;
+	boost::unordered_map<uint64_t, uint64_t> Mboost;
+
+	auto start = timer.now();
 	for (int i = 0; i < KEYS; i++) {
 		uint64_t key = i;
 		uint64_t value = (key * key) + i;
+#if USE_BOOST == 0
 		M.emplace(key, value);
+#else
+		Mboost.emplace(key, value);
+#endif
 	}
-	printf("Insertion done\n");
+	auto end = timer.now();
+
+	double us = duration_cast<microseconds>(end - start).count();
+	printf("Insert tput = %.2f M/s, sum = %lld\n", KEYS / us, sum);
 
 	uint64_t seed = 0xdeadbeef;
 	uint64_t *A = new uint64_t[KEYS];
@@ -42,17 +57,23 @@ int main()
 		A[i] = hrd_fastrand(&seed);
 	}
 
-	std::chrono::high_resolution_clock timer;
-	auto start = timer.now();
+	start = timer.now();
 	for (int i = 0; i < KEYS; i++) {
+#if USE_BOOST == 0
 		auto result = M.find(A[i]);
 		if(result != M.end()) {
 			sum += result->second;
 		}
+#else
+		auto result = Mboost.find(A[i]);
+		if(result != Mboost.end()) {
+			sum += result->second;
+		}
+#endif
 	}
 
-	auto end = timer.now();
-	double us = duration_cast<microseconds>(end - start).count();
+	end = timer.now();
+	us = duration_cast<microseconds>(end - start).count();
 
-	printf("Tput = %.2f M/s, sum = %lld\n", KEYS / us, sum);
+	printf("Get = %.2f M/s, sum = %lld\n", KEYS / us, sum);
 }
