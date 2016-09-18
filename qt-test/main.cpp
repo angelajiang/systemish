61303be93c2b7dd14e03be526080e020a9376aef
Anuj Kalia <anujkaliaiitd@gmail.com>
2016-09-17 23:58:29 -0400

    Edit Qt example a bit

--- qt-test/main.cpp
+++ qt-test/main.cpp
@@ -5,23 +5,30 @@
 class AnalogClockWindow : public RasterWindow
 {
 public:
-    AnalogClockWindow();
+	AnalogClockWindow(bool go_up) : go_up(go_up) {
+		/* Set title */
+		QString title_string;
+		title_string.sprintf("Analog clock %d", go_up);
+		setTitle(title_string);
+
+		/* Add a button */
+		QWidget *wdg = new QWidget(NULL, 0);
+		QPushButton *train_button = new QPushButton(wdg);
+		train_button->setText(tr("something"));
+
+		resize(500, 300);
+
+		m_timerId = startTimer(20);
+	}
 
 protected:
     void timerEvent(QTimerEvent *) Q_DECL_OVERRIDE;
     void render(QPainter *p) Q_DECL_OVERRIDE;
 
 private:
+	bool go_up;
     int m_timerId;
 };
-
-AnalogClockWindow::AnalogClockWindow()
-{
-	setTitle("Analog Clock");
-	resize(200, 200);
-
-	m_timerId = startTimer(1000);
-}
 
 void AnalogClockWindow::timerEvent(QTimerEvent *event)
 {
@@ -32,58 +39,22 @@ void AnalogClockWindow::timerEvent(QTimerEvent *event)
 
 void AnalogClockWindow::render(QPainter *p)
 {
-	static const QPoint hourHand[3] = {
-		QPoint(7, 8),
-		QPoint(-7, 8),
-		QPoint(0, -40)
-	};
-
-	static const QPoint minuteHand[3] = {
-		QPoint(7, 8),
-		QPoint(-7, 8),
-		QPoint(0, -70)
-	};
-
-	QColor hourColor(127, 0, 127);
-	QColor minuteColor(0, 127, 127, 191);
-
 	p->setRenderHint(QPainter::Antialiasing);
 	p->translate(width() / 2, height() / 2);
 
 	int side = qMin(width(), height());
 	p->scale(side / 200.0, side / 200.0);
 
-	p->setPen(Qt::NoPen);
-	p->setBrush(hourColor);
+	//->setPen(Qt::NoPen);
+	//p->setBrush();
 
 	QTime time = QTime::currentTime();
+	int ms = time.msec();
 
-	p->save();
-	p->rotate(30.0 * ((time.hour() + time.minute() / 60.0)));
-	p->drawConvexPolygon(hourHand, 3);
-	p->restore();
-
-	p->setPen(hourColor);
-
-	for (int i = 0; i < 12; ++i) {
-		p->drawLine(88, 0, 96, 0);
-		p->rotate(30.0);
-	}
-
-	p->setPen(Qt::NoPen);
-	p->setBrush(minuteColor);
-
-	p->save();
-	p->rotate(6.0 * (time.minute() + time.second() / 60.0));
-	p->drawConvexPolygon(minuteHand, 3);
-	p->restore();
-
-	p->setPen(minuteColor);
-
-	for (int j = 0; j < 60; ++j) {
-		if ((j % 5) != 0)
-			p->drawLine(92, 0, 96, 0);
-		p->rotate(6.0);
+	if(go_up) {
+		p->drawRect(QRect(0, - 1 * ms / 20, 5, 5));
+	} else {
+		p->drawRect(QRect(0, ms / 20, 5, 5));
 	}
 }
 
@@ -91,8 +62,11 @@ int main(int argc, char **argv)
 {
 	QGuiApplication app(argc, argv);
 
-	AnalogClockWindow clock;
+	AnalogClockWindow clock(true);
 	clock.show();
+
+	AnalogClockWindow clock2(false);
+	clock2.show();
 
 	return app.exec();
 }
