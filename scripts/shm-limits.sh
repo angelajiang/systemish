9c360e62ad21c814774a49e913f1f4948d896dbe
Anuj Kalia <anujkaliaiitd@gmail.com>
2016-12-23 22:56:55 -0500

    Add a script to increase SHM limits

---
+++ scripts/shm-limits.sh
@@ -0,0 +1,4 @@
+# Increase SHM limits
+sudo bash -c "echo kernel.shmmax = 9223372036854775807 >> /etc/sysctl.conf"
+sudo bash -c "echo kernel.shmall = 1152921504606846720 >> /etc/sysctl.conf"
+sudo sysctl -p /etc/sysctl.conf
