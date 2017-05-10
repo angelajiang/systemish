855e46f45b2f3fb4603f1d3ca8e36b2161c1fbfa
Anuj Kalia <akalia@cs.cmu.edu>
2017-05-10 12:49:09 -0600

    Save spdlog work

--- spdlog/example.cc
+++ spdlog/example.cc
@@ -1,49 +1,64 @@
-//
-// Copyright(c) 2015 Gabi Melman.
-// Distributed under the MIT License (http://opensource.org/licenses/MIT)
-//
-//
-// spdlog usage example
-//
-//
+#include <thread>
 #include "spdlog/spdlog.h"
 
-#include <iostream>
-#include <memory>
-
 namespace spd = spdlog;
-int main(int, char* []) {
-  // Console logger with color
-  auto console = spd::stdout_color_mt("console");
-  console->info("Welcome to spdlog!");
-  console->error("Some error message with arg{}..", 1);
 
-  // Formatting examples
-  console->warn("Easy padding in numbers like {:08d}", 12);
-  console->critical(
-      "Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
-  console->info("Support for floats {:03.2f}", 1.23456);
-  console->info("Positional args are {1} {0}..", "too", "supported");
-  console->info("{:<30}", "left aligned");
+static constexpr bool kVerbose = true;
+#define erpc_dwarn(fmt, ...)           \
+  do {                                 \
+    if (kVerbose) {                    \
+      console->warn(fmt, __VA_ARGS__); \
+    }                                  \
+  } while (0)
 
-  spd::get("console")->info(
-      "loggers can be retrieved from a global registry using the "
-      "spdlog::get(logger_name) function");
+#define erpc_dinfo(fmt, ...)           \
+  do {                                 \
+    if (kVerbose) {                    \
+      console->info(fmt, __VA_ARGS__); \
+    }                                  \
+  } while (0)
 
+#define erpc_derror(fmt, ...)           \
+  do {                                  \
+    if (kVerbose) {                     \
+      console->error(fmt, __VA_ARGS__); \
+    }                                   \
+  } while (0)
+
+#define erpc_ddebug(fmt, ...)           \
+  do {                                  \
+    if (kVerbose) {                     \
+      console->debug(fmt, __VA_ARGS__); \
+    }                                   \
+  } while (0)
+
+void console_thread(std::shared_ptr<spd::logger> console) {
   // Customize msg format for all messages
-  spd::set_pattern("*** [%H:%M:%S %z] [thread %t] %v ***");
+  spd::set_pattern("[%H:%M:%S:%f thread %t] eRPC: %v");
 
-  // Runtime log levels
-  spd::set_level(spd::level::info);  // Set global log level to info
-  console->debug("This message shold not be displayed!");
-  console->set_level(spd::level::debug);  // Set specific logger's log level
-  console->debug("This message shold be displayed..");
+  for (size_t i = 0; i < 10; i++) {
+    erpc_derror("Some error message with arg{}..", 1);
+    erpc_dwarn("Easy padding in numbers like {:08d}", 12);
+    erpc_dinfo("Support for floats {:03.2f}", 1.23456);
+    erpc_dinfo("Positional args are {1} {0}..", "too", "supported");
+    erpc_dinfo("{:<30}", "left aligned");
 
-  // Compile time log levels
-  // define SPDLOG_DEBUG_ON or SPDLOG_TRACE_ON
-  SPDLOG_TRACE(console, "Enabled only #ifdef SPDLOG_TRACE_ON..{} ,{}", 1, 3.23);
-  SPDLOG_DEBUG(console, "Enabled only #ifdef SPDLOG_DEBUG_ON.. {} ,{}", 1,
-               3.23);
+    // Runtime log levels
+    spd::set_level(spd::level::info);  // Set global log level to info
+    erpc_ddebug("This message shold not be displayed! {0}", "foo");
+    console->set_level(spd::level::debug);  // Set specific logger's log level
+    erpc_ddebug("This message shold be displayed!, {0}", "bar");
+  }
+}
+
+int main(int, char* []) {
+  std::shared_ptr<spd::logger> console = spd::stdout_color_mt("console");
+
+  auto thread_1 = std::thread(console_thread, console);
+  auto thread_2 = std::thread(console_thread, console);
+
+  thread_1.join();
+  thread_2.join();
 
   // Release and close all loggers
   spd::drop_all();
