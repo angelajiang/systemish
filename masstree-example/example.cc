b10bf30c4b26d857de73e27147bebbf1967d8419
Anuj Kalia <akalia@cs.cmu.edu>
2017-08-18 19:50:28 -0600

    More masstree example work

--- masstree-example/example.cc
+++ masstree-example/example.cc
@@ -2,6 +2,7 @@
 #include "kvrow.hh"
 #include "kvthread.hh"
 #include "masstree.hh"
+#include "masstree_tcursor.hh"
 #include "query_masstree.hh"
 
 volatile mrcu_epoch_type globalepoch = 1;
@@ -10,17 +11,15 @@ volatile bool recovering = false;
 kvtimestamp_t initial_timestamp;
 kvepoch_t global_log_epoch = 0;
 
-template <typename T>
 class mt_index {
  public:
   mt_index() {}
   ~mt_index() { ti_->rcu_stop(); }
 
   void setup() {
-    // ti_ = new threadinfo();
-    // ti_->rcu_start();
     ti_ = threadinfo::make(threadinfo::TI_MAIN, -1);
-    table_ = new T;
+    ti_->rcu_start();
+    table_ = new Masstree::default_table();
     table_->initialize(*ti_);
   }
 
@@ -41,50 +40,6 @@ class mt_index {
                           *ti_);
   }
 
-  bool get_next(const Masstree::Str &cur_key, Masstree::Str &key,
-                Masstree::Str &value) {
-    std::vector<Masstree::Str> keys;
-    std::vector<Masstree::Str> values;
-    lcdf::Json req = lcdf::Json::array(0, 0, cur_key, 2);
-    q_[0].run_scan(table_->table(), req, *ti_);
-    // output_scan(req, keys, values);
-    keys.clear();
-    values.clear();
-
-    for (int i = 2; i != req.size(); i += 2) {
-      keys.push_back(req[i].as_s());
-      values.push_back(req[i + 1].as_s());
-    }
-
-    if ((keys.size() < 2) || (values.size() < 2)) return false;
-    key = keys[1];
-    value = values[1];
-    return true;
-  }
-
-  bool get_next(const char *cur_key, int cur_keylen, Masstree::Str &key,
-                Masstree::Str &value) {
-    std::vector<Masstree::Str> keys;
-    std::vector<Masstree::Str> values;
-    lcdf::Json req =
-        lcdf::Json::array(0, 0, Masstree::Str(cur_key, cur_keylen), 2);
-    q_[0].run_scan(table_->table(), req, *ti_);
-    // output_scan(req, keys, values);
-    keys.clear();
-    values.clear();
-
-    for (int i = 2; i != req.size(); i += 2) {
-      keys.push_back(req[i].as_s());
-      values.push_back(req[i + 1].as_s());
-    }
-
-    if ((keys.size() < 2) || (values.size() < 2)) return false;
-
-    key = keys[1];
-    value = values[1];
-    return true;
-  }
-
   bool remove(const char *key, int keylen) {
     return remove(Masstree::Str(key, keylen));
   }
@@ -94,12 +49,37 @@ class mt_index {
   }
 
  private:
-  T *table_;
+  Masstree::default_table *table_;
   threadinfo *ti_;
   query<row_type> q_[1];
 };
 
 int main() {
-  mt_index<Masstree::default_table> mti;
+  mt_index mti;
   mti.setup();
+
+  std::cout << "\nPUT and GET test\n";
+  mti.put("foo", 3, "bar", 3);
+  mti.put("baz", 3, "bork", 4);
+
+  Str value;
+  bool get_success;
+  get_success = mti.get("foo", 3, value);
+  std::cout << get_success << "\t" << value.s << "\n";
+  get_success = mti.get("bar", 3, value);
+  std::cout << get_success << "\t" << value.s << "\n";
+  get_success = mti.get("dave", 4, value);
+  std::cout << get_success << "\t" << value.s << "\n";
+
+  std::cout << "\nUPDATE test\n";
+  mti.put("foo", 3, "barbar", 6);
+  get_success = mti.get("foo", 3, value);
+  std::cout << get_success << "\t" << value.s << "\n";
+
+  std::cout << "\nREMOVE test\n";
+  mti.remove("foo", 3);
+  get_success = mti.get("foo", 3, value);
+  std::cout << get_success << "\t" << value.s << "\n";
+
+  return 0;
 }
