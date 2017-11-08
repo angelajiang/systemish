5c2caa9c772e77fae0264654bab0e22f061e0905
Xin Zhang <xinzhang1111@gmail.com>
2017-11-08 16:40:07 -0500

    clang-format ds_queue

--- DS/queue/ds_queue.h
+++ DS/queue/ds_queue.h
@@ -1,20 +1,20 @@
 #define DS_QUEUE_DBG 0
 
 #define ds_queue_printf(...) \
-	do { \
-		if(DS_QUEUE_DBG == 1) { \
-			printf(__VA_ARGS__); \
-		} \
-	} while(0)
+  do {                       \
+    if (DS_QUEUE_DBG == 1) { \
+      printf(__VA_ARGS__);   \
+    }                        \
+  } while (0)
 
 struct ds_qnode {
-	int data;
-	struct ds_qnode *next;
+  int data;
+  struct ds_qnode *next;
 };
 
 struct ds_queue {
-	struct ds_qnode *head, *tail;
-	int count;
+  struct ds_qnode *head, *tail;
+  int count;
 };
 
 void ds_queue_init(struct ds_queue *q);
--- DS/queue/main.c
+++ DS/queue/main.c
@@ -1,27 +1,26 @@
-#include<stdlib.h>
-#include<stdio.h>
-#include<assert.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "ds_queue.h"
 
-int main()
-{
-	struct ds_queue q;
-	ds_queue_init(&q);
+int main() {
+  struct ds_queue q;
+  ds_queue_init(&q);
 
-	ds_queue_add(&q, 1);
-	assert(ds_queue_remove(&q) == 1);
+  ds_queue_add(&q, 1);
+  assert(ds_queue_remove(&q) == 1);
 
-	ds_queue_add(&q, 2);
-	ds_queue_add(&q, 3);
-	ds_queue_add(&q, 4);
-	ds_queue_add(&q, 5);
+  ds_queue_add(&q, 2);
+  ds_queue_add(&q, 3);
+  ds_queue_add(&q, 4);
+  ds_queue_add(&q, 5);
 
-	assert(ds_queue_remove(&q) == 2);
-	assert(ds_queue_remove(&q) == 3);
+  assert(ds_queue_remove(&q) == 2);
+  assert(ds_queue_remove(&q) == 3);
 
-	assert(ds_queue_size(&q) == 2);
+  assert(ds_queue_size(&q) == 2);
 
-	printf("Test passed\n");
-	return 0;
+  printf("Test passed\n");
+  return 0;
 }
