6b99e2c34612c1a985a73f3c11bedd191095a7ce
Anuj Kalia <anujkaliaiitd@gmail.com>
2017-12-12 16:52:32 -0500

    Save work

--- raw_roce/receiver.cc
+++ raw_roce/receiver.cc
@@ -6,8 +6,39 @@
 
 #include "common.h"
 
+void install_flow_rule(struct ibv_qp *qp) {
+  static constexpr size_t rule_sz =
+      sizeof(ibv_exp_flow_attr) + sizeof(ibv_exp_flow_spec_eth);
+  static_assert(rule_sz == 64, "");
+
+  uint8_t *flow_rule = new uint8_t[rule_sz];
+  memset(flow_rule, 0, rule_sz);
+
+  auto *flow_attr = reinterpret_cast<struct ibv_exp_flow_attr *>(flow_rule);
+  flow_attr->type = IBV_EXP_FLOW_ATTR_NORMAL;
+  flow_attr->size = 64;
+  flow_attr->priority = 0;
+  flow_attr->num_of_specs = 1;
+  flow_attr->port = 1;
+  flow_attr->flags = 0;
+  flow_attr->reserved = 0;
+
+  auto *flow_spec_eth = reinterpret_cast<struct ibv_exp_flow_spec_eth *>(
+      flow_rule + sizeof(ibv_exp_flow_attr));
+  static_assert(IBV_EXP_FLOW_SPEC_ETH == 0x20, "");
+  flow_spec_eth->type = IBV_EXP_FLOW_SPEC_ETH;
+  static_assert(sizeof(struct ibv_exp_flow_spec_eth) == 0x28, "");
+  flow_spec_eth->size = 0x28;
+  memcpy(flow_spec_eth->val.dst_mac, kDstMAC, 6);
+  memset(flow_spec_eth->mask.dst_mac, 0xff, 6);
+
+  auto *flow = ibv_exp_create_flow(qp, flow_attr);
+  assert(flow != nullptr);
+}
+
 int main() {
   ctrl_blk_t *cb = init_ctx(kDeviceIndex);
+  install_flow_rule(cb->qp);
 
   // Register RX ring memory
   size_t ring_size = kRecvBufSize * kRQDepth;
@@ -34,34 +65,6 @@ int main() {
     int ret = ibv_post_recv(cb->qp, &wr, &bad_wr);
     assert(ret == 0);
   }
-
-  static constexpr size_t rule_sz =
-      sizeof(ibv_exp_flow_attr) + sizeof(ibv_exp_flow_spec_eth);
-  static_assert(rule_sz == 64, "");
-
-  uint8_t *flow_rule = new uint8_t[rule_sz];
-  memset(flow_rule, 0, rule_sz);
-
-  auto *flow_attr = reinterpret_cast<struct ibv_exp_flow_attr *>(flow_rule);
-  flow_attr->type = IBV_EXP_FLOW_ATTR_NORMAL;
-  flow_attr->size = 64;
-  flow_attr->priority = 0;
-  flow_attr->num_of_specs = 1;
-  flow_attr->port = 1;
-  flow_attr->flags = 0;
-  flow_attr->reserved = 0;
-
-  auto *flow_spec_eth = reinterpret_cast<struct ibv_exp_flow_spec_eth *>(
-      flow_rule + sizeof(ibv_exp_flow_attr));
-  static_assert(IBV_EXP_FLOW_SPEC_ETH == 0x20, "");
-  flow_spec_eth->type = IBV_EXP_FLOW_SPEC_ETH;
-  static_assert(sizeof(struct ibv_exp_flow_spec_eth) == 0x28, "");
-  flow_spec_eth->size = 0x28;
-  memcpy(flow_spec_eth->val.dst_mac, kDstMAC, 6);
-  memset(flow_spec_eth->mask.dst_mac, 0xff, 6);
-
-  auto *flow = ibv_exp_create_flow(cb->qp, flow_attr);
-  assert(flow != nullptr);
 
   printf("Listening\n");
   while (true) {
