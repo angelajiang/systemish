b2b55083c2325815ae0e31ed78b9b9781ff49c35
Anuj Kalia <anujkaliaiitd@gmail.com>
2017-12-18 23:19:14 -0500

    Reorg raweth utils

--- raw_eth/utils/inet_hdrs.h
+++ raw_eth/utils/inet_hdrs.h
@@ -3,10 +3,22 @@
 
 #include <arpa/inet.h>
 #include <assert.h>
+#include <dirent.h>
+#include <errno.h>
+#include <ifaddrs.h>
+#include <linux/if_arp.h>
+#include <linux/if_packet.h>
+#include <net/ethernet.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <string.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include <sstream>
 #include <string>
+#include <vector>
 
 #define _unused(x) ((void)(x))  // Make production build happy
 #define likely(x) __builtin_expect(!!(x), 1)
@@ -31,7 +43,7 @@ std::string mac_to_string(const uint8_t mac[6]) {
   return ret.str();
 }
 
-uint32_t ip_from_str(const char* ip) {
+uint32_t ip_from_str(const char *ip) {
   uint32_t addr;
   int ret = inet_pton(AF_INET, ip, &addr);
   rt_assert(ret == 1, "inet_pton() failed");
@@ -41,10 +53,69 @@ uint32_t ip_from_str(const char* ip) {
 std::string ip_to_string(uint32_t ipv4_addr) {
   static_assert(INET_ADDRSTRLEN == 16, "");
   char str[INET_ADDRSTRLEN];
-  const char* ret = inet_ntop(AF_INET, &ipv4_addr, str, sizeof(str));
+  const char *ret = inet_ntop(AF_INET, &ipv4_addr, str, sizeof(str));
   rt_assert(ret == str, "inet_ntop failed");
   str[INET_ADDRSTRLEN - 1] = 0;  // Null-terminate
   return str;
+}
+
+static std::string get_interface_ipv4_str(std::string interface) {
+  struct ifaddrs *ifaddr, *ifa;
+
+  int ret = getifaddrs(&ifaddr);
+  assert(ret == 0);
+
+  for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) {
+    if (ifa->ifa_addr->sa_family != AF_INET) continue;  // IP address
+    if (strcmp(ifa->ifa_name, interface.c_str()) != 0) continue;
+
+    auto sin_addr = reinterpret_cast<sockaddr_in *>(ifa->ifa_addr);
+    uint32_t ipv4_addr = *reinterpret_cast<uint32_t *>(&sin_addr->sin_addr);
+    std::string ip_str = ip_to_string(ipv4_addr);
+
+    freeifaddrs(ifaddr);
+    return ip_str;
+  }
+
+  freeifaddrs(ifaddr);
+  return "";
+}
+
+static std::string get_interface_mac_str(std::string interface) {
+  struct ifreq ifr;
+  ifr.ifr_addr.sa_family = AF_INET;
+  strncpy(ifr.ifr_name, interface.c_str(), IFNAMSIZ - 1);
+
+  int fd = socket(AF_INET, SOCK_DGRAM, 0);
+  assert(fd >= 0);
+  int ret = ioctl(fd, SIOCGIFHWADDR, &ifr);
+  rt_assert(ret == 0, "MAC address IOCTL failed");
+
+  close(fd);
+  return mac_to_string(reinterpret_cast<uint8_t *>(ifr.ifr_hwaddr.sa_data));
+}
+
+static std::string get_ibdev_ipv4_string(std::string ibdev_name) {
+  std::string dev_dir = "/sys/class/infiniband/" + ibdev_name + "/device/net";
+
+  std::vector<std::string> net_ifaces;
+  DIR *dp;
+  struct dirent *dirp;
+  dp = opendir(dev_dir.c_str());
+  assert(dp != nullptr);
+
+  while (true) {
+    dirp = readdir(dp);
+    if (dirp == nullptr) break;
+
+    if (strcmp(dirp->d_name, ".") == 0) continue;
+    if (strcmp(dirp->d_name, "..") == 0) continue;
+    net_ifaces.push_back(std::string(dirp->d_name));
+  }
+  closedir(dp);
+
+  assert(net_ifaces.size() > 0);
+  return get_interface_ipv4_str(net_ifaces[0]);
 }
 
 #endif  // INET_HDRS_H
--- raw_eth/utils/main.cc
+++ raw_eth/utils/main.cc
@@ -3,20 +3,6 @@
  * @brief Test utils for raw Ethernet
  */
 
-#include <assert.h>
-#include <dirent.h>
-#include <errno.h>
-#include <ifaddrs.h>
-#include <linux/if_arp.h>
-#include <linux/if_packet.h>
-#include <net/ethernet.h>
-#include <stdio.h>
-#include <string.h>
-#include <sys/ioctl.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <vector>
 #include "inet_hdrs.h"
 
 void test_ip_to_str() {
@@ -42,65 +28,6 @@ void test_mac_to_string() {
   printf("mac address to string = %s\n", mac_to_string(mac).c_str());
 }
 
-std::string get_interface_ipv4_str(std::string interface) {
-  struct ifaddrs *ifaddr, *ifa;
-
-  int ret = getifaddrs(&ifaddr);
-  assert(ret == 0);
-
-  for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) {
-    if (ifa->ifa_addr->sa_family != AF_INET) continue;  // IP address
-    if (strcmp(ifa->ifa_name, interface.c_str()) != 0) continue;
-
-    auto sin_addr = reinterpret_cast<sockaddr_in *>(ifa->ifa_addr);
-    uint32_t ipv4_addr = *reinterpret_cast<uint32_t *>(&sin_addr->sin_addr);
-    std::string ip_str = ip_to_string(ipv4_addr);
-
-    freeifaddrs(ifaddr);
-    return ip_str;
-  }
-
-  freeifaddrs(ifaddr);
-  return "";
-}
-
-std::string get_interface_mac_str(std::string interface) {
-  struct ifreq ifr;
-  ifr.ifr_addr.sa_family = AF_INET;
-  strncpy(ifr.ifr_name, interface.c_str(), IFNAMSIZ - 1);
-
-  int fd = socket(AF_INET, SOCK_DGRAM, 0);
-  assert(fd >= 0);
-  int ret = ioctl(fd, SIOCGIFHWADDR, &ifr);
-  rt_assert(ret == 0, "MAC address IOCTL failed");
-
-  close(fd);
-  return mac_to_string(reinterpret_cast<uint8_t *>(ifr.ifr_hwaddr.sa_data));
-}
-
-std::string get_ibdev_ipv4_addr(std::string ibdev_name) {
-  std::string dev_dir = "/sys/class/infiniband/" + ibdev_name + "/device/net";
-
-  std::vector<std::string> net_ifaces;
-  DIR *dp;
-  struct dirent *dirp;
-  dp = opendir(dev_dir.c_str());
-  assert(dp != nullptr);
-
-  while (true) {
-    dirp = readdir(dp);
-    if (dirp == nullptr) break;
-
-    if (strcmp(dirp->d_name, ".") == 0) continue;
-    if (strcmp(dirp->d_name, "..") == 0) continue;
-    net_ifaces.push_back(std::string(dirp->d_name));
-  }
-  closedir(dp);
-
-  assert(net_ifaces.size() > 0);
-  return get_interface_ipv4_str(net_ifaces[0]);
-}
-
 int main() {
   test_ip_to_str();
   test_str_to_ip();
@@ -124,7 +51,7 @@ int main() {
 
   std::string ibdev_arr[] = {"mlx5_0", "mlx5_1", "mlx5_2", "mlx5_3"};
   for (auto s : ibdev_arr) {
-    std::string ip_str = get_ibdev_ipv4_addr(s);
+    std::string ip_str = get_ibdev_ipv4_string(s);
     printf("IB device %s, IP %s\n", s.c_str(),
            ip_str.length() != 0 ? ip_str.c_str() : "N/A");
   }
