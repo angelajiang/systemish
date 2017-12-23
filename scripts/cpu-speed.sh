b9318e201107ae61b834dc88f679dae7c159984a
Anuj Kalia <anujkaliaiitd@gmail.com>
2017-12-22 19:44:08 -0500

    Fix CPU frequency command

--- scripts/cpu-speed.sh
+++ scripts/cpu-speed.sh
@@ -1,6 +1,6 @@
 # Print the current CPU GHz for all physical cores in this system
 for i in `seq 0 15`; do
-	freq=`sudo cat /sys/devices/system/cpu/cpu$i/cpufreq/cpuinfo_cur_freq`
+	freq=`sudo cat /sys/devices/system/cpu/cpu$i/cpufreq/scaling_cur_freq`
 	echo "CPU $i: $freq KHz"
 done
 
