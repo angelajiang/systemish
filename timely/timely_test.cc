31a8854707e430f0193f95390bb76c6777081ce6
Anuj Kalia <akalia@cs.cmu.edu>
2018-01-21 15:22:47 -0700

    Handle rtt > T_high

--- timely/timely_test.cc
+++ timely/timely_test.cc
@@ -9,6 +9,7 @@ static double kTimelyMinRate = 5.0 * 1000 * 1000;
 static double kTimelyAddRate = 5.0 * 1000 * 1000;
 static double kMinRTT = 2;
 static double kTLow = 50;
+static double kTHigh = 1000;
 static double kEwmaAlpha = .875;
 static double kBeta = .008;
 
@@ -28,17 +29,22 @@ class Timely {
 
   void update_rate(double sample_rtt) {
     assert(sample_rtt > kTLow);
-
-    double rtt_diff = sample_rtt - prev_rtt;
-    prev_rtt = sample_rtt;
-    avg_rtt_diff = ((1 - kEwmaAlpha) * avg_rtt_diff) + (kEwmaAlpha * rtt_diff);
-
     double new_rate;
-    double norm_grad = avg_rtt_diff / kMinRTT;
-    double weight = w(norm_grad);
-    double error = (sample_rtt - kTLow) / kTLow;
-    new_rate =
-        rate * (1 - kBeta * weight * error) + kTimelyAddRate * (1 - weight);
+
+    if (sample_rtt <= kTHigh) {
+      double rtt_diff = sample_rtt - prev_rtt;
+      prev_rtt = sample_rtt;
+      avg_rtt_diff =
+          ((1 - kEwmaAlpha) * avg_rtt_diff) + (kEwmaAlpha * rtt_diff);
+
+      double norm_grad = avg_rtt_diff / kMinRTT;
+      double weight = w(norm_grad);
+      double error = (sample_rtt - kTLow) / kTLow;
+      new_rate =
+          rate * (1 - kBeta * weight * error) + kTimelyAddRate * (1 - weight);
+    } else {
+      new_rate = rate * (1 - kBeta * (1 - kTHigh / sample_rtt));
+    }
 
     rate = std::max(new_rate, rate * 0.5);
     rate = std::min(rate, kTimelyMaxRate);
@@ -55,11 +61,13 @@ void test(size_t rtt) {
   double P = kTimelyAddRate * kTLow / (kBeta * (rtt - kTLow));  // Predict
 
   Timely timely;
-  for (size_t i = 0; i < 20000; i++) timely.update_rate(rtt);
-  printf("mean %zu us, steady tput %.2f Gbps, prediction = %.2f\n", rtt,
+  for (size_t i = 0; i < 20000; i++) {
+    timely.update_rate(rtt + rand() % 40);
+  }
+  printf("RTT %zu us, steady tput %.2f Gbps, prediction = %.2f\n", rtt,
          Timely::rate_to_gbps(timely.rate), Timely::rate_to_gbps(P));
 }
 
 int main() {
-  for (size_t iter = 1; iter <= 20; iter++) test(kTLow + iter * 10);
+  for (size_t iter = 1; iter <= 20; iter++) test(kTLow + iter * 20);
 }
