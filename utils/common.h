ed926ea9268f0bf9f834049a2b1b8743b9c1a502
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-02-08 05:41:07 -0500

    Add utils to systemish

---
+++ utils/common.h
@@ -0,0 +1,41 @@
+#include <assert.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <cerrno>
+#include <limits>
+#include <mutex>
+#include <sstream>
+#include <string>
+#include <thread>
+#include <vector>
+
+#define _unused(x) ((void)(x))  // Make production build happy
+#define likely(x) __builtin_expect(!!(x), 1)
+#define unlikely(x) __builtin_expect(!!(x), 0)
+
+#define KB(x) (static_cast<size_t>(x) << 10)
+#define KB_(x) (KB(x) - 1)
+#define MB(x) (static_cast<size_t>(x) << 20)
+#define MB_(x) (MB(x) - 1)
+
+/// Check a condition at runtime. If the condition is false, throw exception.
+static inline void rt_assert(bool condition, std::string throw_str) {
+  if (unlikely(!condition)) throw std::runtime_error(throw_str);
+}
+
+/// Check a condition at runtime. If the condition is false, print error message
+/// and exit.
+static inline void exit_assert(bool condition, std::string error_msg) {
+  if (unlikely(!condition)) {
+    fprintf(stderr, "%s. Exiting.\n", error_msg.c_str());
+    fflush(stderr);
+    exit(-1);
+  }
+}
+
+/// Check a condition at runtime. If the condition is false, throw exception.
+static inline void rt_assert(bool condition) {
+  if (unlikely(!condition)) throw std::runtime_error("Error");
+}
