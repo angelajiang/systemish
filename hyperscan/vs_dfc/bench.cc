5214563e104650c7e9304fa5c70b29c79995d871
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-02-08 06:30:13 -0500

    Add segfaulting dfc bench

--- hyperscan/vs_dfc/bench.cc
+++ hyperscan/vs_dfc/bench.cc
@@ -6,6 +6,8 @@
 #include <fstream>
 #include <string>
 
+#include "dfc_src/dfc.h"
+#include "dfc_src/dfc_framework.h"
 #include "utils/timer.h"
 
 static constexpr size_t kNumPkts = 30000;
@@ -23,19 +25,8 @@ static int event_handler(unsigned int, unsigned long long, unsigned long long,
 }
 
 void evaluate_hyperscan() {
-  match_count = 0;
-
   hs_database_t *db;
   hs_compile_error_t *compile_err;
-
-  std::ifstream in("virus.txt");
-  while (true) {
-    std::string s;
-    std::getline(in, s);
-    if (s.empty()) break;
-    virus_vec.push_back(s);
-    printf("Virus: %s\n", s.c_str());
-  }
 
   for (std::string &virus : virus_vec) {
     int ret =
@@ -81,20 +72,60 @@ void evaluate_hyperscan() {
   hs_free_database(db);
 }
 
+void evaluate_dfc() {
+  DFC_STRUCTURE *dfc = DFC_New();
+
+  size_t pattern_id = 0;
+  for (std::string &virus : virus_vec) {
+    DFC_AddPattern(dfc, (unsigned char *)virus.c_str(), strlen(virus.c_str()),
+                   0, pattern_id++);
+  }
+
+  DFC_Compile(dfc);
+
+  TscTimer timer;
+  timer.start();
+
+  size_t num_pkts_matched = 0;
+  for (size_t i = 0; i < kNumPkts; i++) {
+    match_count = 0;
+    int res = DFC_Search(dfc, (unsigned char *)pkt_vec[i], kPktSize, nullptr);
+    num_pkts_matched += (res > 0);
+  }
+
+  timer.stop();
+
+  printf("DFC: number of matches: %zu, bandwidth = %.3f GB/s\n",
+         num_pkts_matched,
+         (kPktSize * kNumPkts) / (1000000000.0 * timer.avg_sec(freq_ghz)));
+}
+
 int main() {
   freq_ghz = measure_rdtsc_freq();
   printf("Kicking up TurboBoost. freq_ghz = %.2f\n", freq_ghz);
   nano_sleep(2000000000, freq_ghz);
   printf("Starting work!\n");
 
+  // Get the list of viruses
+  std::ifstream in("virus.txt");
+  while (true) {
+    std::string s;
+    std::getline(in, s);
+    if (s.empty()) break;
+    virus_vec.push_back(s);
+  }
+  printf("Number of virus = %zu\n", virus_vec.size());
+
+  // Generate random packets
   pkt_vec.resize(kNumPkts);
   for (size_t i = 0; i < kNumPkts; i++) {
     pkt_vec[i] = new char[kPktSize];
     for (size_t j = 0; j < kPktSize; j++) {
-      pkt_vec[i][j] = '0' + (rand() % 10);
+      pkt_vec[i][j] = 'a' + (rand() % 25);
     }
   }
 
   evaluate_hyperscan();
+  evaluate_dfc();
   return 0;
 }
--- hyperscan/vs_dfc/gen_virus.sh
+++ hyperscan/vs_dfc/gen_virus.sh
@@ -1,6 +1,6 @@
 num_virus=10000
 
 for i in `seq 1 $num_virus`; do
-  virus=`pwgen 32 1`
+  virus=`pwgen 2 1`
   echo $virus >> virus.txt
 done
