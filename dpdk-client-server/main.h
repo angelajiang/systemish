c81db2a6a5ef54e0e19a1b37566418b73f5055e9
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-05-13 16:08:56 -0400

    clang-format dpdk client-server example

--- dpdk-client-server/main.h
+++ dpdk-client-server/main.h
@@ -1,48 +1,48 @@
-#include <stdio.h>
-#include <stdarg.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdint.h>
-#include <inttypes.h>
-#include <sys/types.h>
-#include <sys/queue.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
-#include <setjmp.h>
-#include <stdarg.h>
+#include <assert.h>
 #include <ctype.h>
 #include <errno.h>
 #include <getopt.h>
+#include <inttypes.h>
+#include <setjmp.h>
+#include <stdarg.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/queue.h>
+#include <sys/shm.h>
+#include <sys/types.h>
 #include <unistd.h>
-#include <assert.h>
 
-#include <rte_common.h>
-#include <rte_log.h>
-#include <rte_memory.h>
-#include <rte_memcpy.h>
-#include <rte_memzone.h>
-#include <rte_tailq.h>
-#include <rte_eal.h>
-#include <rte_per_lcore.h>
-#include <rte_launch.h>
 #include <rte_atomic.h>
-#include <rte_cycles.h>
-#include <rte_prefetch.h>
-#include <rte_lcore.h>
-#include <rte_per_lcore.h>
 #include <rte_branch_prediction.h>
-#include <rte_interrupts.h>
-#include <rte_pci.h>
-#include <rte_random.h>
-#include <rte_debug.h>
-#include <rte_ether.h>
-#include <rte_ip.h>
-#include <rte_udp.h>
-#include <rte_ethdev.h>
-#include <rte_ring.h>
-#include <rte_mempool.h>
-#include <rte_mbuf.h>
 #include <rte_byteorder.h>
+#include <rte_common.h>
+#include <rte_cycles.h>
+#include <rte_debug.h>
+#include <rte_eal.h>
+#include <rte_ethdev.h>
+#include <rte_ether.h>
+#include <rte_interrupts.h>
+#include <rte_ip.h>
+#include <rte_launch.h>
+#include <rte_lcore.h>
+#include <rte_log.h>
+#include <rte_mbuf.h>
+#include <rte_memcpy.h>
+#include <rte_memory.h>
+#include <rte_mempool.h>
+#include <rte_memzone.h>
+#include <rte_pci.h>
+#include <rte_per_lcore.h>
+#include <rte_per_lcore.h>
+#include <rte_prefetch.h>
+#include <rte_random.h>
+#include <rte_ring.h>
+#include <rte_tailq.h>
+#include <rte_udp.h>
 
 #include "sizes.h"
 
@@ -51,7 +51,7 @@
 // sizeof(rte_mbuf) = 64, RTE_PKTMBUF_HEADROOM = 128
 #define MBUF_SIZE (2048 + sizeof(struct rte_mbuf) + RTE_PKTMBUF_HEADROOM)
 #define DEFAULT_PACKET_LENGTH 60
-#define NB_MBUF   8192
+#define NB_MBUF 8192
 #define NB_MBUF_CACHE 512
 
 #define RX_PTHRESH 8 /**< Default values of RX prefetch threshold reg. */
@@ -78,30 +78,35 @@
 
 #define ISSET(a, i) (a & (1 << i))
 #define MAX(a, b) (a > b ? a : b)
-#define htons(n) (((((unsigned short)(n) & 0xFF)) << 8) | (((unsigned short)(n) & 0xFF00) >> 8))
+#define htons(n) \
+  (((((unsigned short)(n)&0xFF)) << 8) | (((unsigned short)(n)&0xFF00) >> 8))
 
-
-
-#define CPE2(val, msg, error, fault) \
-	if(val) {fflush(stdout); rte_exit(EXIT_FAILURE, msg, error, fault);}
-#define CPE(val, msg) \
-	if(val) {fflush(stdout); rte_exit(EXIT_FAILURE, msg);}
+#define CPE2(val, msg, error, fault)           \
+  if (val) {                                   \
+    fflush(stdout);                            \
+    rte_exit(EXIT_FAILURE, msg, error, fault); \
+  }
+#define CPE(val, msg)            \
+  if (val) {                     \
+    fflush(stdout);              \
+    rte_exit(EXIT_FAILURE, msg); \
+  }
 
 #define GHZ_CPS 1000000000
 // Cycles to nanoseconds conversion constants
-#define C_FAC ((double) GHZ_CPS / XIA_R0_CPS)
-#define S_FAC ((double) GHZ_CPS / XIA_R2_CPS)
+#define C_FAC ((double)GHZ_CPS / XIA_R0_CPS)
+#define S_FAC ((double)GHZ_CPS / XIA_R2_CPS)
 
 #define LOG_CAP M_256
 #define LOG_CAP_ M_256_
 #define BASE_HT_LOG_SHM_KEY 1
 
 // All xia hardware specific features need to be prefixed by XIA_
-#define XIA_R0_PORT_MASK 0xc	// xge2,3
-#define XIA_R0_CPS 2270000000	// Client cycles per second
+#define XIA_R0_PORT_MASK 0xc   // xge2,3
+#define XIA_R0_CPS 2270000000  // Client cycles per second
 
-#define XIA_R2_PORT_MASK 0xf0	// xge4,5,6,7
-#define XIA_R2_CPS 2700000000	// Server cycles per second
+#define XIA_R2_PORT_MASK 0xf0  // xge4,5,6,7
+#define XIA_R2_CPS 2700000000  // Server cycles per second
 
 // On all our machines, even numbered lcores are on socket 0
 #define LCORE_TO_SOCKET(lcore) (lcore % 2)
@@ -123,10 +128,11 @@ void check_all_ports_link_status(uint8_t port_num, int portmask);
 void print_buf(char *A, int n);
 
 void run_server(int *ht_log, struct rte_mempool **l2fwd_pktmbuf_pool);
-void run_client(int client_id, int *ht_log, struct rte_mempool **l2fwd_pktmbuf_pool);
+void run_client(int client_id, int *ht_log,
+                struct rte_mempool **l2fwd_pktmbuf_pool);
 int *shm_alloc(int key, int cap);
 
-inline uint32_t fastrand(uint64_t* seed);
+inline uint32_t fastrand(uint64_t *seed);
 void micro_sleep(int us, double cycles_to_ns_fac);
 
 void set_mac(uint8_t *mac_ptr, LL mac_addr);
