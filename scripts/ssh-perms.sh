09fc86854de0c73b1b83bdb4e64d263d8bd2a24d
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-08-01 19:09:46 -0700

    add script to fix ssh folder permissions

---
+++ scripts/ssh-perms.sh
@@ -0,0 +1,7 @@
+# Fix ssh file permissions. This is sometimes needed to fix passwordless ssh.
+chmod 700 ~/.ssh
+chmod 644 ~/.ssh/authorized_keys
+chmod 644 ~/.ssh/known_hosts
+chmod 644 ~/.ssh/config
+chmod 600 ~/.ssh/id_rsa
+chmod 644 ~/.ssh/id_rsa.pub
