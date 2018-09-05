96f59af108581e0d676cf673904a8ac408846bbd
Anuj Kalia <anujkaliaiitd@gmail.com>
2018-09-05 16:33:42 -0700

    improve shuffle bench

--- bench/shuffle/main.cc
+++ bench/shuffle/main.cc
@@ -3,9 +3,9 @@
 #include <x86intrin.h>
 #include "../common.h"
 
-constexpr size_t kNum8BBlocks = 16384;
+constexpr size_t kNum8BBlocks = MB(32);
 
-int main() {
+void bench_sse() {
   typedef __m128 State;
   __m128i transitions[256];  // Garbage transisions are ok
   uint8_t data[8];
@@ -14,15 +14,15 @@ int main() {
   clock_gettime(CLOCK_REALTIME, &start);
 
   __m128i s;
-  for (size_t i = 0; i < 8; i++) {
-    uint8_t c1 = data[i + 0];
-    uint8_t c2 = data[i + 1];
-    uint8_t c3 = data[i + 2];
-    uint8_t c4 = data[i + 3];
-    uint8_t c5 = data[i + 4];
-    uint8_t c6 = data[i + 5];
-    uint8_t c7 = data[i + 6];
-    uint8_t c8 = data[i + 7];
+  for (size_t iter = 0; iter < kNum8BBlocks; iter++) {
+    uint8_t c1 = data[0];
+    uint8_t c2 = data[1];
+    uint8_t c3 = data[2];
+    uint8_t c4 = data[3];
+    uint8_t c5 = data[4];
+    uint8_t c6 = data[5];
+    uint8_t c7 = data[6];
+    uint8_t c8 = data[7];
     s = _mm_shuffle_epi8(transitions[c1], s);
     s = _mm_shuffle_epi8(transitions[c2], s);
     s = _mm_shuffle_epi8(transitions[c3], s);
@@ -33,6 +33,12 @@ int main() {
     s = _mm_shuffle_epi8(transitions[c8], s);
   }
 
+  uint8_t *proof_of_work = reinterpret_cast<uint8_t *>(&s);
   double ns = ns_since(start);
-  printf("ns = %.2f\n", ns);
+  printf("ns per byte = %.2f, proof_of_work = %u\n", ns / (kNum8BBlocks * 8),
+         *proof_of_work);
+}
+
+int main() {
+  for (size_t i = 0; i < 10; i++) bench_sse();
 }
